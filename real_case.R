78db39dc52238a760d5a3ac23f412ad7483b333a
Meng Huang <meng.huang@wsu.edu>
2022-10-22 12:58:05 -0500

    Update real_case.R

--- real_case.R
+++ real_case.R
@@ -1,13 +1,13 @@
 #please replace all the pathway in this script to the pathway saved your data
 #create a new map file for PLINK binary format to replace allele from A/B to G/C/A/T
 setwd("~/Downloads")
-map=read.table("/Users/mh0986/Dropbox/PROJECTS/GWAS_protocol/HDRA-G6-4-SNP-MAP/HDRA-G6-4-final-snp-map.tsv",head=F)
-bim=read.table("~/Dropbox/PROJECTS/GWAS_protocol/HDRA-G6-4-RDP1-RDP2-NIAS/HDRA-G6-4-RDP1-RDP2-NIAS.bim",head=F)
+map=read.table("HDRA-G6-4-SNP-MAP/HDRA-G6-4-final-snp-map.tsv",head=F)
+bim=read.table("HDRA-G6-4-RDP1-RDP2-NIAS.bim",head=F)
 bim[,5:6]=map[,4:5]
-write.table(bim,"~/Dropbox/PROJECTS/GWAS_protocol/HDRA-G6-4-RDP1-RDP2-NIAS/HDRA-G6-4-RDP1-RDP2-NIAS.bim",quote=F,row.names = F,col.names = F)
+write.table(bim,"HDRA-G6-4-RDP1-RDP2-NIAS.bim",quote=F,row.names = F,col.names = F)
 
 #convert PLINK format to VCF format
-system("./plink2 --bfile ~/Dropbox/PROJECTS/GWAS_protocol/HDRA-G6-4-RDP1-RDP2-NIAS/HDRA-G6-4-RDP1-RDP2-NIAS --export vcf --out rice")
+system("./plink2 --bfile HDRA-G6-4-RDP1-RDP2-NIAS --export vcf --out rice")
 
 #run beagle5 to impute missing genotype. beagle5 could be downloaded from https://faculty.washington.edu/browning/beagle/beagle.html
 system("java -Xmx20000m -jar beagle.jar gt=rice.vcf out=test")
@@ -17,15 +17,15 @@ system("./plink2 --vcf test.vcf.gz --export A --out rice")
 
 #load SNP data, map, and phenotype
 snp=read.table("rice.raw",head=T)
-myGM=read.table("~/Dropbox/PROJECTS/GWAS_protocol/HDRA-G6-4-RDP1-RDP2-NIAS/HDRA-G6-4-RDP1-RDP2-NIAS.bim",head=F)[,c(2,1,4)]
-trait1=read.table("/Users/mh0986/Dropbox/PROJECTS/GWAS_protocol/phenoAvLen_G6_4_RDP12_ALL/phenoAvLen_G6_4_RDP12_ALL.txt",head=T)
+myGM=read.table("HDRA-G6-4-RDP1-RDP2-NIAS.bim",head=F)[,c(2,1,4)]
+trait1=read.table("phenoAvLen_G6_4_RDP12_ALL.txt",head=T)
 
 #match and convert sample ID between SNP data and phenotype data
 ID=snp[,1:2]
 write.table(ID,"temp.txt",quote=F,row.names = F,col.names = F)
 ID=read.table("temp.txt",head=F,sep='_')
 trait1[,1]=paste("IRGC",trait1[,1],sep='')
-trait=read.delim("~/Dropbox/PROJECTS/GWAS_protocol/HDRA-G6-4-RDP1-RDP2-NIAS/HDRA-G6-4-RDP1-RDP2-NIAS-sativa-only.sample_map.rev2.tsv",head=F)
+trait=read.delim("HDRA-G6-4-RDP1-RDP2-NIAS-sativa-only.sample_map.rev2.tsv",head=F)
 trait=merge(trait,ID,by.x="V2",by.y="V2")
 trait=merge(trait,trait1,by.x="V3",by.y="FID")
 myGD=snp[,-c(1,3:6)]
